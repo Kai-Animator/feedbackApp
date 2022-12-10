867a2273cd5204bc6d895b76b5135f77a85221e6
Kai-Animator <kaireml@protonmail.com>
2022-12-11 02:39:32 +0900

    update React Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const title = 'My blog';
+  const body = 'This is my blog post'({});
+  const comments = [
+    {
+      id: 1,
+      text: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.',
+    },
+    {
+      id: 2,
+      text: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.',
+    },
+    {
+      id: 3,
+      text: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.',
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div classList='container'>
+        <h1>{title}</h1>
+        <p>{body}</p>
+        <div>
+          // <h3>Comments ({comments.length})</h3>
+          <ul>
+            {comments.map((comment, index) => (
+              <li key={index}>{comment.text}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
